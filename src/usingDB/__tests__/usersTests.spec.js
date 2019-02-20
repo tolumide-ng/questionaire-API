fc5019603a2c113c3d2a2e99a37415d7e4a9b0e3
Tolumide <tolumideshopein@gmail.com>
2019-02-20 06:49:24 +0100

    Updates usertests/login route

--- src/usingDB/__tests__/usersTests.spec.js
+++ src/usingDB/__tests__/usersTests.spec.js
@@ -40,7 +40,7 @@ describe('login/comments', () => {
     it('should return the users details with a 200 status code', (done) => {
         chai.request(server)
             .post('/v1/users/login/')
-            .send({ user: 1 })
+            .send({ user: 1, password: 'tomilola' })
             .end((err, res) => {
                 res.should.have.status(200);
                 res.should.be.json;
